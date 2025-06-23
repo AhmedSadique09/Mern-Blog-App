ca60e3f9ef227881fe5444f1c2137c526c2c49f8
Ghulam Ahmed <dev.ahmed47@gmail.com>
2025-06-23 02:42:00 -0700

    Add Admin functionality to user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -43,7 +43,7 @@ export const signin = async (req, res, next) => {
         if(!isPasswordValid){
             return next(errorHandler(400,'Invalid password'));
         }
-        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+        const token = jwt.sign({id: validUser._id, isAdmin:validUser.isAdmin}, process.env.JWT_SECRET, {expiresIn: '1d'});
         const {password:pass, ...rest} = validUser._doc;
        res.status(200).cookie('access_token', token, {
   httpOnly: true,
@@ -65,7 +65,7 @@ export const google = async (req, res, next) => {
     const user = await User.findOne({ email });
 
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
       const { password, ...rest } = user._doc;
 
       return res
@@ -92,7 +92,7 @@ export const google = async (req, res, next) => {
 
       await newUser.save();
 
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
       const { password, ...rest } = newUser._doc;
 
       res
